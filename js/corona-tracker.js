7512702f0fa83e2560cabb4ac83b9ba898d02e4b
eync <eync@users.noreply.github.com>
2020-09-24 09:56:39 +0300

    Update: continents with chartsjs, needs to be fixed later on and make better functions

--- js/corona-tracker.js
+++ js/corona-tracker.js
@@ -15,7 +15,6 @@ const main = async () => {
     for (const itsMe in data) {
         countries.push(itsMe, data[itsMe])
         
-        
     }
 
     return data
@@ -63,4 +62,62 @@ const listItems = async () => {
     
 } 
 
+//data.datasets[0].data.push(40)
+//data.labels.push(40)
+
+
+const url2 = 'https://disease.sh/v3/covid-19/continents';
+const continents = async () => {
+    //if the API is down or broken, throw error.
+    const response = await fetch(url2)
+    if (response.status < 200 || response.status >= 400) {
+      throw new Error("Something is wrong with API server!")
+    }
+    //wait until response
+    const data = await response.json();
+    //for the received data and push in to the array 
+    let continentNames = [];
+    let continentDeads = [];
+    for (const itsContinent in data) {
+        continentNames.push(data[itsContinent].continent);
+        continentDeads.push(data[itsContinent].deaths);
+        // console.log(JSON.stringify(data[itsContinent].continent, null, 2));
+    }
+    let continentDeathsString = Object.keys(data[0])[3];
+    drawCharters(continentNames, continentDeads, continentDeathsString);
+    return data;
+
+  }
+
+
+const drawCharters = (continentName, continentDead, continentDeathsStrn) => {
+
+const ctx = document.getElementById('myChart');
+
+myChart = new Chart(ctx, {
+    type: 'doughnut',
+data: {
+    labels: continentName,
+    animateScale: true,
+    datasets: [{
+        label: continentDeathsStrn,
+        data: continentDead,
+        
+        backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        hoverOffset: 4
+        
+    }]
+}
+
+});
+}
+continents();
+drawCharters();
 listItems();
